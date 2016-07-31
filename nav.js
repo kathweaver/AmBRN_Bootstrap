820c7499b76f105088d73e92b124a0e4075b16af
Kathleen Weaver <kathleen@kweaver.org>
2016-07-31 13:09:45 -0500

    Implemented website with bootstrap and javascript
    Fully tested and loaded to production

---
+++ nav.js
@@ -0,0 +1,21 @@
+document.write(
+"   <nav class='navbar navbar-default navbar-fixed-top'>" +
+"     <div class='navbar-inner'>" + 
+"      <div class='container'> "+
+"        <ul class='nav'>" +
+"           <li><a href='./#top'>Home</a></li>" +
+"           <li><a href='./#contact'>Contact and Board Information</a></li>" +
+"           <li><a href='./memorial.html'>Memorial</a></li>" +
+"           <li><a href='./prevention.html'>Prevention Articles</a></li>" +
+"           <li><a href='./Newsletter/2014_June/index.html'>Newsletter</a></li>" +
+"        </ul>" +
+"        <form class='navbar-form navbar-right' action='https://www.paypal.com/cgi-bin/webscr' method='post' target='_top'>" +
+"           <input type='hidden' name='cmd' value='_s-xclick'>" +
+"           <input type='hidden' name='hosted_button_id' value='VBP6FWGVFFTTU'>" +
+"           <input type='image' src='https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif' border='0' name='submit' alt='PayPal - The safer, easier way to pay online!'> "+
+"           <img alt='donate' border='0' src='https://www.paypalobjects.com/en_US/i/scr/pixel.gif' width='1' height='1'>" +
+"        </form>" +
+"       </div>" +
+"     <div>" +
+"    </nav>"  
+); 
